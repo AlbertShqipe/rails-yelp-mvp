de42e9e65f02d6693e9ab4567e7ae446cd039b99
Albert Nikolli <albert_nikolli@icloud.com>
2024-02-04 13:19:15 +0100

    Created 5 restaurants

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,12 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+puts 'Destroying existing records...'
+Restaurant.destroy_all
+puts 'Creating new records...'
+Restaurant.create(name: "Mamma Lisa", address: 'Roma', category: 'italian')
+Restaurant.create(name: 'Wok Walkie', address: 'Beijing', category: 'chinese')
+Restaurant.create(name: 'Shitta Foodda', address: 'Brussel', category: 'belgian')
+Restaurant.create(name: 'Galette des Galettes', address: 'Lyon', category: 'french')
+Restaurant.create(name: 'Sushi Ishu', address: 'Tokyo', category: 'japanese')
+puts 'Records created!'
